c6abd2c688c3036affbd2bae9a2284b967dacc85
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-28 13:42:20 +0300

    Fix

--- Python-middle/main.py
+++ Python-middle/main.py
@@ -7,11 +7,20 @@ import os
 # print(os.getcwd())
 # os.mkdir("It works")
 # os.mkdir(os.path.join(path,"1"))
+#
+# path = os.path.normpath("C:\\Users\\Redmi\\Desktop\\dir\\1")
+# os.chdir(path)
+# print(os.getcwd())
+# with open("Text.txt", "r") as file:
+#     # file.write("Привет ,что то там ,но это работает")
+#     a = file.read()
+#     print(a)
 
-path = os.path.normpath("C:\\Users\\Redmi\\Desktop\\dir\\1")
-os.chdir(path)
 print(os.getcwd())
-with open("Text.txt", "r") as file:
-    # file.write("Привет ,что то там ,но это работает")
-    a = file.read()
-    print(a)
+path = os.path.normpath("C:\\Users\Redmi\Documents\Programming\StepExercises")
+for path1,dirname,file, in os.walk(path):
+        print("{:*^20}".format("Path"))
+        print(f"\t{path1}")
+        print("{:#^40}".format("Files"))
+        print(f"\t\t{file}")
+
