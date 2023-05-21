bd9d4fc2454129efd0d4d004f9dbde4c8001f218
Krim-code <suhetsky.gricha555@gmail.com>
2023-05-21 19:30:41 +0300

    i'm so tired

---
+++ Python-middle/Exercise2.py
@@ -0,0 +1,11 @@
+name = "Tom"
+age = 16
+gender = "male"
+
+print(f"{name:*^50}")
+print(f"{name:*>50}")
+print(f"{name:*<50}")
+
+print(f"Name - {name} \n"
+      f"Age - {age} \n"
+      f"Gender - {gender}")
